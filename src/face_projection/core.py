3c331d3789692f2d3d46950382d180a2663f9e2c
Tim Büchner <tim.buechner@uni-jena.de>
2023-02-15 14:11:51 +0100

    Add docstrings and comments

--- src/face_projection/core.py
+++ src/face_projection/core.py
@@ -10,6 +10,16 @@ from .face_model import FaceModel
 
 
 class Warper:
+    """Warper class.
+
+    This class is used to warp the face image to the face model.
+    It uses the face model to create a canvas and then uses the landmarks of the face
+    if not provided they will be calculated using media pipe.
+
+    This classes should be only instantiated once and then reused as it holds the
+    internal buffers for faster processing.
+    """
+
     def __init__(self, scale: float = 1.0) -> None:
         self.face_model = FaceModel()
         self.face_model.set_scale(scale)
@@ -33,9 +43,11 @@ class Warper:
         self.depth_buffer = np.empty(self.len_triangles)
 
     def width(self) -> int:
+        """Get the width of the face model."""
         return self.face_model.width
 
     def height(self) -> int:
+        """Get the height of the face model."""
         return self.face_model.height
 
     def set_scale(self, scale: float) -> None:
@@ -51,10 +63,27 @@ class Warper:
 
         This will create a canvas for the face model based on the current scale of the
         face model.
+
+        Returns:
+            np.ndarray[np.int8]: A canvas for the face model.
+
         """
         return self.face_model.create_canvas()
 
-    def get_landmarks(self, face_img: np.ndarray) -> np.ndarray:
+    def get_landmarks(self, face_img: np.ndarray):
+        """Get the landmarks of the face image.
+
+        Writes the landmarks of the face image into the internal landmarks buffer.
+
+        TODO: This should be moved somewhere else.
+
+        Args:
+            face_img (np.ndarray): The face image
+
+        Returns:
+            none
+
+        """
         # this should be put somewhere else
         # locate the face with media pipe
         h, w = face_img.shape[:2]
@@ -66,60 +95,84 @@ class Warper:
 
     def apply(
         self,
-        face_img: np.ndarray[np.int8],
-        face_data: np.ndarray[np.int8],
-        landmarks: Optional[np.ndarray[np.int32]] = None,
+        img_face: np.ndarray[np.uint8],
+        img_data: np.ndarray[np.uint8],
+        lms_face: Optional[np.ndarray[np.int32]] = None,
         beta: float = 0.2,
-    ) -> np.ndarray[np.int8]:
-        if not isinstance(face_img, np.ndarray):
-            raise TypeError("face_img must be a numpy array")
+    ) -> np.ndarray[np.uint8]:
+        """Warps the face data onto the face image.
 
-        if not isinstance(face_data, np.ndarray):
-            raise TypeError("face_data must be a numpy array")
+        This function warps the face data onto the face image. The face data is warped
+        according to the landmarks of the face image. The landmarks of the face image
+        are computed with the internal face model.
+
+        Args:
+            img_face (np.ndarray[np.uint8]): The face image
+            img_data (np.ndarray[np.uint8]): The data which is warped onto the face image
+            lms_face (Optional[np.ndarray[np.int32]], optional): The landmarks of the face image. Defaults to None.
+            beta (float, optional): Blending parameter. Defaults to 0.2.
+
+        Raises:
+            TypeError: If the img_face is not a numpy array
+            TypeError: If the img_data is not a numpy array
+            TypeError: If the beta is not a float between 0 and 1
+            ValueError: If the img_data is not valid for the face model
+            TypeError: If the landmarks are not a numpy array
+
+        Returns:
+            np.ndarray[np.uint8]: The warped face image
+        """
+        if not isinstance(img_face, np.ndarray):
+            raise TypeError("img_face must be a numpy array")
+
+        if not isinstance(img_data, np.ndarray):
+            raise TypeError("img_dat must be a numpy array")
 
         if not isinstance(beta, float) or (beta < 0 or beta > 1):
             raise TypeError("beta must be a float between 0 and 1")
 
-        if not self.face_model.check_valid(face_data):
-            raise ValueError(f"face_data is not valid for the face model, expected shape: [{self.face_model.height, self.face_model.width, 3}]")
+        if not self.face_model.check_valid(img_data):
+            raise ValueError(f"img_dat is not valid for the face model, expected shape: [{self.face_model.height, self.face_model.width, 3}]")
 
-        if landmarks is None:
-            self.get_landmarks(face_img)
+        if lms_face is None:
+            self.get_landmarks(img_face)
         else:
-            if not isinstance(landmarks, np.ndarray):
+            if not isinstance(lms_face, np.ndarray):
                 raise TypeError("landmarks must be a numpy array")
-            if landmarks.shape[0] != 468:
+            if lms_face.shape[0] != 468:
                 raise ValueError("landmarks must have 468 landmarks")
-            self.__landmarks = landmarks
+            self.__landmarks = lms_face
 
         return self.__warp(
             cooridnates_dst=self.__landmarks[self.face_model.masking],
-            image_src=face_data,
-            image_dst=face_img,
+            image_src=img_data,
+            image_dst=img_face,
             beta=beta,
         )
 
     def __warp(
         self,
         cooridnates_dst: np.ndarray[np.int32],
-        image_src: np.ndarray[np.int8],
-        image_dst: np.ndarray[np.int8],
-        beta: float = 0.2,
+        image_src: np.ndarray[np.uint8],
+        image_dst: np.ndarray[np.uint8],
+        beta: float = 0.3,
     ) -> np.ndarray[np.int8]:
         """Warps triangulated area from one image to another image
 
+        Uses the internal face model for triangulation and landmarks.
+        The interval buffers are allocated in the constructor once and reused for performance reasons.
+
         Args:
-            coordiantes_src (np.ndarray[np.float32]): Triangle coordinates source image
-            cooridnates_dst (np.ndarray[np.float32]): Triangle coordiantes destination image
-            triangles (np.ndarray[np.int32]): Trigulation (array with corner indices)
-            image_src (np.ndarray[np.int8]): Source image to take from
-            image_dst (np.ndarray[np.int8]): Destination iamge to copy to (is deep copied)
-            beta (float, optional): Blending parameter. Defaults to 0.2.
+            cooridnates_dst (np.ndarray[np.float32]): Landmarks of the destination image
+            image_src (np.ndarray[np.int8]): Image which is warped onto the destination image
+            image_dst (np.ndarray[np.int8]): Destination image where the source image is warped onto
+            beta (float, optional): Blending parameter. Defaults to 0.3.
 
         Returns:
-            np.ndarray[np.int8]: _description_
+            np.ndarray[np.int8]:  The warped image of the destination image
         """
         image_out = image_dst.copy()
+        # Compute affine transform between src and dst triangles
         for idx_tri in range(self.len_triangles):
             tri_src = self.face_model.points[self.face_model.triangles[idx_tri]]
             tri_dst = cooridnates_dst[self.face_model.triangles[idx_tri]]
@@ -142,8 +195,10 @@ class Warper:
             self.buffer_3_2[:, 1] = tri_dst[:, 1] - rect_dst[1]
             self.tri_dst_crop_buffer[idx_tri] = self.buffer_3_2
 
+        # Sort triangles by depth (furthest to nearest)
         self.depth_buffer = np.argsort(self.depth_buffer)[::-1]
 
+        # Warp triangles from src image to dst image
         for idx in range(self.len_triangles):
             i = self.depth_buffer[idx]
             # Crop input image
